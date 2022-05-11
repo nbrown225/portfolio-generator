692cf8420f3f327aac8a58132bbaf6c242c1da27
nbrown225 <chbrown225@yahoo.com>
2022-05-11 07:08:06 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+// var message = 'WADDUP NODE!';
+// var sum = 4 + 4;
+
+
+// console.log(message);
+// console.log(sum);
+
+// var commandLineArgs = process.argv;
+// console.log(commandLineArgs);
+
+
+const profileDataArgs = process.argv.slice(2);
+
+const printProfileData = profileDataArr => {
+  // This...
+  for (let i = 0; i < profileDataArr.length; i += 1) {
+    console.log(profileDataArr[i]);
+  }
+
+  console.log('================');
+
+  // Is the same as this...
+  profileDataArr.forEach(profileItem => console.log(profileItem));
+};
+
+printProfileData(profileDataArgs);
