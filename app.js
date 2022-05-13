4ef32c3e6ad37b616c17e09099a050196920043a
nbrown225 <chbrown225@yahoo.com>
2022-05-13 12:24:49 -0700

    added questions, for loop to add more projects

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const inquirer = require('inquirer');
 console.log(inquirer);
 
-inquirer
-  .prompt([
+const promptUser = () => {
+  return inquirer.prompt([
     {
       type: 'input', 
       name: 'name', 
@@ -12,10 +12,83 @@ inquirer
       type: 'input',
       name: 'job', 
       message: 'WHAT IS YOUR JOB?'
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username'
+    },
+    {
+      type: 'input',
+      name: 'about',
+      message: 'Provide some information about yourself:'
+    }
+
+  ])
+}
+const promptProject = (portfolioData) => {
+  console.log(`
+  =================
+  Add a New Project
+  =================
+  `);
+  if (!portfolioData.projects) {
+    portfolioData.projects = [];
+  }
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is the name of your project?'
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a description of the project (Required)'
+    },
+    {
+      type: 'checkbox',
+      name: 'languages',
+      message: 'What did you build this project with? (Check all that apply)',
+      choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+    },
+    {
+      type: 'input',
+      name: 'link',
+      message: 'Enter the GitHub link to your project. (Required)'
+    },
+    {
+      type: 'confirm',
+      name: 'feature',
+      message: 'Would you like to feature this project?',
+      default: false
+    },
+    {
+      type: 'confirm',
+      name: 'confirmAddProject',
+      message: 'Would you like to enter another project?',
+      default: false
     }
   ])
-  .then(answers => console.log(answers));
-  
+  // In the preceding expression, we use the array method push() to place the projectData from inquirer into the new projects array we just created.
+.then(projectData => {
+  portfolioData.projects.push(projectData);
+  if (projectData.confirmAddProject) {
+    return promptProject(portfolioData);
+  } else {
+    return portfolioData;
+  }
+});
+};
+
+ // call multiple functions with .then
+ promptUser()
+ .then(promptProject)
+ .then(portfolioData => {
+   console.log(portfolioData);
+ })
+
+
 // const fs = require('fs'); 
 // const generatePage = require('./src/page-template');
 
